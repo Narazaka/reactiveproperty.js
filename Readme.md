8de5694317ea198307af20fe58dc0acedc255dd0
Narazaka <info@narazaka.net>
2017-02-25 13:13:10 +0900

    1.0.0

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# [reactiveproperty.js - ReactiveProperty for RxJS](https://github.com/Narazaka/reactiveproperty.js)
+
+Partial javascript implementation of https://github.com/runceel/ReactiveProperty.
+
+## Synopsys
+
+```typescript
+import { ReactiveProperty } from "reactiveproperty";
+
+class MyClass {
+    myProperty: ReactiveProperty<string>;
+
+    constructor(str: string) {
+        this.myProperty = new ReactiveProperty<string>(str);
+    }
+}
+
+const obj = new MyClass("foo");
+obj.myProperty.subscribe(str => console.log(str));
+// > foo
+obj.myProperty.value = "bar";
+// > bar
+```
+
+## See also
+
+- [reactiveproperty-rxjs](https://github.com/paveldk/reactiveproperty-rxjs)
+
+## License
+
+This is released under [MIT License](http://narazaka.net/license/MIT?2017).
---
+++ src/reactiveproperty.ts
@@ -0,0 +1,85 @@
+import {Subject} from "rxjs";
+import {Subscribable} from "rxjs/Observable";
+import {PartialObserver} from "rxjs/Observer";
+import {ISubscription, Subscription} from "rxjs/subscription";
+import {toSubscriber} from "rxjs/util/toSubscriber";
+
+export interface IReactiveProperty<T> extends Subscribable<T>, ISubscription {
+    value: T;
+}
+
+export type ReactivePropertyMode = {
+    /** If next value is same as current, not set and not notify. */
+    distinctUntilChanged?: boolean;
+    /** Push notify on instance created and subscribed. */
+    raiseLatestValueOnSubscribe?: boolean;
+};
+
+export class ReactiveProperty<T> implements IReactiveProperty<T> {
+    closed = false;
+    private latestValue: T;
+    private readonly isDistinctUntilChanged: boolean;
+    private readonly isRaiseLatestValueOnSubscribe: boolean;
+
+    private readonly source = new Subject<T>();
+
+    constructor(
+        initialValue?: T,
+        mode: ReactivePropertyMode = {distinctUntilChanged: true, raiseLatestValueOnSubscribe: true},
+    ) {
+        if (initialValue) this.latestValue = initialValue;
+
+        this.isRaiseLatestValueOnSubscribe = Boolean(mode.raiseLatestValueOnSubscribe);
+        this.isDistinctUntilChanged = Boolean(mode.distinctUntilChanged);
+    }
+
+    /** Get latestValue or push(set) value. */
+    get value(): T {
+        return this.latestValue;
+    }
+
+    set value(value: T) {
+        if (this.latestValue == null || value == null) {
+            if (this.isDistinctUntilChanged && this.latestValue && value == null)  return;
+            this.setValue(value);
+            return;
+        }
+
+        if (this.isDistinctUntilChanged && this.latestValue === value) return;
+
+        this.setValue(value);
+    }
+
+    /** Subscribe source. */
+    subscribe(): Subscription;
+    subscribe(observer: PartialObserver<T>): Subscription;
+    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): Subscription;
+    subscribe(
+        observerOrNext?: PartialObserver<T> | ((value: T) => void),
+        error?: (error: any) => void,
+        complete?: () => void,
+    ) {
+        const observer = toSubscriber(observerOrNext, error, complete);
+        if (this.isRaiseLatestValueOnSubscribe) observer.next(this.latestValue);
+        return this.source.subscribe(observer);
+    }
+
+    /** Unsubscribe all subscription. */
+    unsubscribe() {
+        if (this.closed) return;
+
+        this.closed = true;
+        this.source.complete();
+        this.source.unsubscribe();
+    }
+
+    /** Invoke next. */
+    forceNotify() {
+        this.setValue(this.latestValue);
+    }
+
+    private setValue(value: T) {
+        this.latestValue = value;
+        this.source.next(value);
+    }
+}
